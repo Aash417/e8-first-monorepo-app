1a64272f91f179182a08b677d51336101cd63524
aashish_k <aashishkathat00@gmail.com>
2024-04-11 16:32:40 +0530

    adding react front end project

---
+++ apps/web/app/user/page.tsx
@@ -0,0 +1,14 @@
+import Admin from '@repo/ui/admin';
+import { Box } from '@repo/ui/box';
+
+function page() {
+	return (
+		<>
+			<div>page</div>
+			<Admin />
+			<Box>working</Box>
+		</>
+	);
+}
+
+export default page;
---
+++ packages/ui/src/box.tsx
@@ -0,0 +1,8 @@
+export const Box = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>Box Component</h1>
+      {children}
+    </div>
+  );
+};
